f70c07096d4bceb6f934373aee318ca6b10a3135
Anshuman Goswami <anshumangoswami07@gmail.com>
2023-06-05 20:43:22 +0530

    small updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,32 @@
-'use client';
-import { useState } from 'react'
-import { Dialog } from '@headlessui/react'
-import Image from 'next/image';
+"use client";
+import { useState } from "react";
+import { Dialog } from "@headlessui/react";
+import Image from "next/image";
 import Icon from "../public/logo/logo-transparent-svg.svg";
 import { useRouter } from "next/navigation";
 
 const navigation = [
-  { name: 'Product', href: '#' },
-  { name: 'Features', href: '#' },
-  { name: 'Company', href: '#' },
-  { name: 'FAQ', href: '#' },
-]
+  { name: "Product", href: "#" },
+  { name: "Features", href: "#" },
+  { name: "Company", href: "#" },
+  { name: "FAQ", href: "#" },
+];
 
 export default function Home() {
   const router = useRouter();
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  // console.log(`${process.env.NEXT_PUBLIC_PROJECTID}`);
 
   return (
     <div className="bg-white">
       <header className="absolute inset-x-0 top-0 z-50">
-        <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+        <nav
+          className="flex items-center justify-between p-6 lg:px-8"
+          aria-label="Global"
+        >
           <div className="flex lg:flex-1">
-            <Image
-              src={Icon}
-              height={200}
-              width={200}
-              alt="Product Logo"
-            />
+            <Image src={Icon} height={200} width={200} alt="Product Logo" />
           </div>
           <div className="flex lg:hidden">
             <button
@@ -35,19 +35,20 @@ export default function Home() {
               onClick={() => setMobileMenuOpen(true)}
             >
               <span className="sr-only">Open main menu</span>
-
             </button>
           </div>
           <div className="hidden lg:flex lg:gap-x-12">
             {navigation.map((item) => (
-              <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
+              <a
+                key={item.name}
+                href={item.href}
+                className="text-sm font-semibold leading-6 text-gray-900"
+              >
                 {item.name}
               </a>
             ))}
           </div>
         </nav>
-
-
       </header>
 
       <div className="relative isolate px-6 pt-14 lg:px-8">
@@ -59,15 +60,19 @@ export default function Home() {
             className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
             }}
           />
         </div>
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              Wish to know how to host a great event? {' '}
-              <a href="https://medium.com/hackonhackathon/hackathon-to-dos-101-272485ba4a8c" target="_blank" className="font-semibold text-[#DB195A]">
+              Wish to know how to host a great event?{" "}
+              <a
+                href="https://medium.com/hackonhackathon/hackathon-to-dos-101-272485ba4a8c"
+                target="_blank"
+                className="font-semibold text-[#DB195A]"
+              >
                 <span className="absolute inset-0" aria-hidden="true" />
                 Read more <span aria-hidden="true">&rarr;</span>
               </a>
@@ -78,15 +83,22 @@ export default function Home() {
               Community to the resuce 🚀
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">
-              Events are a great way to connect with like-minded people. They also provide a great opportunity to learn and network with others in your field. Get started with Event-Ally to host or join your next event.
+              Events are a great way to connect with like-minded people. They
+              also provide a great opportunity to learn and network with others
+              in your field. Get started with Event-Ally to host or join your
+              next event.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <button
                 className="rounded-md bg-[#DB195A] px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-[#DB195A] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                onClick={() => router.push('/login')}>
+                onClick={() => router.push("/login")}
+              >
                 Get started
               </button>
-              <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+              <a
+                href="#"
+                className="text-sm font-semibold leading-6 text-gray-900"
+              >
                 Learn more <span aria-hidden="true">→</span>
               </a>
             </div>
@@ -100,11 +112,11 @@ export default function Home() {
             className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#DB195A] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
             }}
           />
         </div>
       </div>
     </div>
-  )
+  );
 }
