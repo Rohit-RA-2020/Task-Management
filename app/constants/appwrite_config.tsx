3181a6212be7305cb0ee2ca1b786374889e9a31a
Rohit Ranjan <ranjanrohit812@gmail.com>
2023-06-06 16:22:23 +0530

    Env Setup & Github log added

--- app/constants/appwrite_config.tsx
+++ app/constants/appwrite_config.tsx
@@ -1,117 +1,9 @@
 import { Client, Account, Models, ID, Databases, Storage } from "appwrite";
 import { User } from "./interface";
-import sdk, { Permission, Role } from "node-appwrite";
-
-class ServerConfig {
-  client: sdk.Client = new sdk.Client();
-  databases: sdk.Databases = new sdk.Databases(this.client);
-  key: string = `${process.env.NEXT_PUBLIC_APIKEY}`;
-
-  constructor() {
-    this.client
-      .setEndpoint("https://cloud.appwrite.io/v1")
-      .setProject(`${process.env.NEXT_PUBLIC_PROJECTID}`)
-      .setKey(this.key);
-  }
-
-  createColl(collId: string, name: string): void {
-    try {
-      this.databases.createCollection(
-        `${process.env.NEXT_PUBLIC_PROJECTID}`,
-        ID.custom(collId),
-        name,
-        [
-          Permission.read(Role.any()), // Anyone can view this document
-          Permission.update(Role.any()), // Writers can update this document
-          Permission.create(Role.any()), // Admins can update this document
-          Permission.delete(Role.any()), // Admins can delete this document
-        ]
-      );
-    } catch (error) {
-      console.log(error);
-    } finally {
-      this.databases.createStringAttribute(
-        `${process.env.NEXT_PUBLIC_PROJECTID}`,
-        collId,
-        "EventName",
-        256,
-        false
-      );
-      this.databases.createStringAttribute(
-        `${process.env.NEXT_PUBLIC_PROJECTID}`,
-        collId,
-        "Date",
-        256,
-        false
-      );
-      this.databases.createStringAttribute(
-        `${process.env.NEXT_PUBLIC_PROJECTID}`,
-        collId,
-        "Tech",
-        256,
-        false
-      );
-      this.databases.createIntegerAttribute(
-        `${process.env.NEXT_PUBLIC_PROJECTID}`,
-        collId,
-        "Attendees",
-        false
-      );
-      this.databases.createIntegerAttribute(
-        `${process.env.NEXT_PUBLIC_PROJECTID}`,
-        collId,
-        "Price",
-        false
-      );
-      this.databases.createStringAttribute(
-        `${process.env.NEXT_PUBLIC_PROJECTID}`,
-        collId,
-        "Venue",
-        100,
-        false
-      );
-      this.databases.createStringAttribute(
-        `${process.env.NEXT_PUBLIC_PROJECTID}`,
-        collId,
-        "Speakers",
-        256,
-        false
-      );
-      this.databases.createStringAttribute(
-        `${process.env.NEXT_PUBLIC_PROJECTID}`,
-        collId,
-        "Sponsors",
-        100,
-        false
-      );
-      this.databases.createBooleanAttribute(
-        `${process.env.NEXT_PUBLIC_PROJECTID}`,
-        collId,
-        "Approval",
-        false,
-        false
-      );
-      this.databases.createStringAttribute(
-        `${process.env.NEXT_PUBLIC_PROJECTID}`,
-        collId,
-        "Description",
-        256,
-        false
-      );
-      this.databases.createStringAttribute(
-        `${process.env.NEXT_PUBLIC_PROJECTID}`,
-        collId,
-        "Audience",
-        256,
-        false
-      );
-    }
-  }
-}
 
 class AppwriteConfig {
-  databaseId: string = `${process.env.NEXT_PUBLIC_PROJECTID}`;
-  bannerBucket: string = `${process.env.NEXT_PUBLIC_PROJECTID}`;
+  databaseId: string = `${process.env.NEXT_PUBLIC_DATABASEID}`;
+  activeCollId: string = `${process.env.NEXT_PUBLIC_EVENT_COLLID}`;
 
   client: Client = new Client();
   account: Account = new Account(this.client);
@@ -120,6 +12,7 @@ class AppwriteConfig {
   user: User = {} as User;
 
   constructor() {
+    console.log(process.env.NEXT_PUBLIC_PROJECTID)
     this.client
       .setEndpoint("https://cloud.appwrite.io/v1")
       .setProject(`${process.env.NEXT_PUBLIC_PROJECTID}`);
@@ -200,42 +93,29 @@ class AppwriteConfig {
     Description: string,
     Audience: string
   ): void {
-    console.log("Event function called", this.user.$id);
+    console.log("Event function called");
 
     try {
-      this.account
-        .get()
-        .then((res) => {
-          this.user = res;
-          const serverConfig = new ServerConfig();
-          serverConfig.createColl(this.user.$id, this.user.name);
-          console.table(this.user);
-          try {
-            const promise = this.databases.createDocument(
-              this.databaseId,
-              this.user.$id,
-              ID.unique(),
-              {
-                EventName: EventName,
-                Date: Date,
-                Tech: Tech,
-                Attendees: Attendees,
-                Price: Price,
-                Venue: Venue,
-                Speakers: Speakers,
-                Sponsors: Sponsors,
-                Approval: Approval,
-                Description: Description,
-                Audience: Audience,
-              }
-            );
-          } catch (error) {
-            console.log(error);
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+      const promise = this.databases.createDocument(
+        this.databaseId,
+        this.activeCollId,
+        ID.unique(),
+        {
+          EventName: EventName,
+          Date: Date,
+          Tech: Tech,
+          Attendees: Attendees,
+          Price: Price,
+          Venue: Venue,
+          Speakers: Speakers,
+          Sponsors: Sponsors,
+          Approval: Approval,
+          Description: Description,
+          Audience: Audience,
+        }
+      ).then((res) => {
+        console.table(res);
+      });
     } catch (error) {
       console.log(error);
     }
