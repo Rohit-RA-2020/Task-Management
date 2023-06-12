f4880a0e044bdeede3112224c8dbbb5e1f593d02
Rohit Ranjan <ranjanrohit812@gmail.com>
2023-06-12 18:10:43 +0530

    List Attendees

--- app/stats/[event]/page.tsx
+++ app/stats/[event]/page.tsx
@@ -7,26 +7,21 @@ import AppwriteConfig from "../../constants/appwrite_config";
 import { useRouter } from "next/navigation";
 import Header from "@/app/components/header";
 
-export default function Stat({ params }: { params: { reg: string } }) {
+export default function Event({ params }: { params: { event: string } }) {
   const appwriteConfig = new AppwriteConfig();
 
+  const [docs, setDocs] = useState<Models.Document[]>();
   const [loader, setLoader] = useState(false);
-
-  const [reg, setReg] = useState<Models.Document[]>();
 
   const router = useRouter();
 
   useEffect(() => {
     setLoader(true);
-    const getRegs = appwriteConfig.databases
-      .listDocuments(
-        `6486b2d946e0fe5fdbf4`,
-        `6486ef21982b518ccb6b`
-      )
+    const getDocs = appwriteConfig.databases
+      .listDocuments(`${process.env.NEXT_PUBLIC_REGDB}`, params["event"])
       .then(
         function (response) {
-          setReg(response.documents);
-          console.log(response)
+          setDocs(response.documents);
         },
         function (error) {
           console.log(error);
@@ -36,35 +31,33 @@ export default function Stat({ params }: { params: { reg: string } }) {
   }, []);
 
   return (
-    <div>
+    <div className="bg-gray-100 min-h-screen">
       <Header />
       <div className="max-w-7xl mx-auto">
-        <p className="text-3xl font-bold mb-2 text-center mx-auto py-5">
-          All Active Registrations
-        </p>
-        {loader ? (
-          <p className="mx-auto text-red-700">Loading....</p>
-        ) : (
-          <div className="py-10">
-            {reg &&
-              reg.map((item) => (
-                <div key={item.$id} className="py-2">
-                  <section className="text-gray-600 body-font">
-                    <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-                      <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-                        <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-                          {item.name}
-                        </h1>
-                        <p className="mb-8 leading-relaxed">
-                          {item.email}
-                        </p>
-                      </div>
-                    </div>
-                  </section>
+      <p className="text-2xl font-bold mb-2 flex justify-center mx-auto py-10">
+        Active Registrations
+      </p>
+      {loader ? (
+        <p className="mx-auto text-red-700">Loadings....</p>
+      ) : (
+        <div>
+          {docs &&
+            docs.map((item) => (
+              <div key={item.$id}>
+                <div className="p-4 flex items-center justify-between">
+                  <div>
+                    <p>{item.name}</p>
+                  </div>
+                  <div>
+                    <span className="text-[#2f394a] cursor pointer">
+                      {item.email}
+                    </span>
+                  </div>
                 </div>
-              ))}
-          </div>
-        )}
+              </div>
+            ))}
+        </div>
+      )}
       </div>
     </div>
   );
